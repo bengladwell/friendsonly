36ccdb3dd7e0c32149d29d181d8fb29069e3df9c
Ben Gladwell <bengladwell@gmail.com>
2016-04-13 08:24:22 -0400

    proper streaming from S3

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ var express = require('express'),
   _ = require('underscore'),
   request = require('request'),
   querystring = require('querystring'),
-  P = require('bluebird'),
   AWS = require('aws-sdk'),
   config = require('./config.json'),
   media = require('./media.json');
@@ -187,49 +186,79 @@ app.get('/oauth', function (req, res) {
 });
 
 app.get('/media/*', function (req, res) {
-  var s3 = P.promisifyAll(new AWS.S3({
+  var s3 = new AWS.S3({
       accessKeyId: config.aws.accessKeyId,
       secretAccessKey: config.aws.secretAccessKey,
       logger: process.stdout
-    })),
-    key = req.params[0];
+    }),
+    key = req.params[0],
+    addToHeaders = {},
+    s3req,
+    readStream;
 
   if (!key) {
     res.sendStatus(400);
     return false;
   }
 
-  s3.headObjectAsync({
+  if (req.get('if-none-match')) {
+    addToHeaders.IfNoneMatch = req.get('if-none-match');
+  }
+  if (req.get('if-match')) {
+    addToHeaders.IfMatch = req.get('if-match');
+  }
+  if (req.get('if-modified-since')) {
+    addToHeaders.IfModifiedSince = new Date(req.get('if-modified-since'));
+  }
+  if (req.get('if-unmodified-since')) {
+    addToHeaders.IfUnmodifiedSince = req.get('if-unmodified-since');
+  }
+  if (req.get('range')) {
+    addToHeaders.Range = req.get('range');
+  }
+
+  s3req = s3.getObject(_.extend({
     Bucket: config.aws.Bucket,
-    Key: key
-  }).then(function (head) {
+    Key: req.params[0]
+  }, addToHeaders));
 
-    // handle ETag caching
-    if (req.get('If-None-Match') === head.ETag) {
-      res.sendStatus(304);
-      return true;
+  s3req.on('httpHeaders', function (code, headers) {
+
+    res.status(code);
+
+    if (headers['content-type'] === 'application/octet-stream' && _.contains(['.mp4', '.webm', '.ogv'], path.extname(key))) {
+      if (path.extname(key) === '.mp4') {
+        res.set('Content-Type', 'video/mp4');
+      }
+    } else if (headers['content-type']) {
+      res.set('Content-Type', headers['content-type']);
     }
-
-    res.set('Content-Length', head.ContentLength);
-    res.set('ETag', head.ETag);
-    res.set('Cache-Control', 'max-age=172800'); // 2 days
-
-    // stream videos
-    if (head.ContentType === 'application/octet-stream' && path.extname(key) === '.mp4') {
-      res.set('Content-Type', 'video/mp4');
-    } else {
-      res.set('Content-Type', head.ContentType);
-
+    if (headers['last-modified']) {
+      res.set('Last-Modified', headers['last-modified']);
     }
-
-    s3.getObject({
-      Bucket: config.aws.Bucket,
-      Key: req.params[0]
-    }).createReadStream().pipe(res);
-
-  }, function (err) {
-    res.sendStatus(err.statusCode);
+    if (headers.etag) {
+      res.set('ETag', headers.etag);
+    }
+    if (headers['accept-ranges']) {
+      res.set('Accept-Ranges', headers['accept-ranges']);
+    }
+    if (headers['content-range']) {
+      res.set('Content-Range', headers['content-range']);
+    }
+    if (headers['content-length']) {
+      res.set('Content-Length', headers['content-length']);
+    }
   });
+
+  readStream = s3req.createReadStream().on('error', function (err) {
+    if (err.statusCode === 304) {
+      return res.status(304).end();
+    }
+    return res.status(err.statusCode).end();
+  });
+
+  readStream.pipe(res);
+
 });
 
 app.listen(3000);
