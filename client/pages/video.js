a62c42be947e2dd9af3b8ee8082eb60078ad3c85
Ben Gladwell <bengladwell@gmail.com>
2018-09-22 14:18:22 -0400

    Wait until video is ready to change src

--- client/pages/video.js
+++ client/pages/video.js
@@ -1,10 +1,13 @@
 "use strict";
 const player = document.getElementById('player'),
-  width = window.innerHeight > window.innerWidth ? window.innerHeight : window.innerWidth,
-  match = player.currentSrc.match(/^(.+)\-\d+\.(.+)$/);
+  width = window.innerHeight > window.innerWidth ? window.innerHeight : window.innerWidth;
 
-if (width > 1280) {
-  player.src = match[1] + '-720.' + match[2];
-} else if (width > 854) {
-  player.src = match[1] + '-480.' + match[2];
-}
+player.addEventListener('loadedmetadata', () => {
+  const match = player.currentSrc.match(/^(.+)\-\d+\.(.+)$/);
+  if (width > 1280) {
+    player.src = match[1] + '-720.' + match[2];
+  } else if (width > 854) {
+    player.src = match[1] + '-480.' + match[2];
+  }
+}, { once: true });
+
